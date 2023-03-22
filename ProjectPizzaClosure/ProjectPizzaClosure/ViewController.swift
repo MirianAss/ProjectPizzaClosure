22316d01cff614107a5c725d2d3b464e21a717cb
Mirian Santana <miriansantana93@gmail.com>
2023-03-22 11:27:02 -0300

    Added Alamofire and SDWebImage

---
+++ ProjectPizzaClosure/ProjectPizzaClosure/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProjectPizzaClosure
+//
+//  Created by Mirian Santana on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btStart(_ sender: Any) {
+    }
+    
+}
+
